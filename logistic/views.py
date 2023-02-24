1f066b9bfe7e9526e7db8bcb0736da09e236d996
maks <max.iw15@yandex.ru>
2023-02-24 18:39:44 +0300

    finish work

--- logistic/views.py
+++ logistic/views.py
@@ -7,10 +7,10 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    search_fields = ["title", "description"]
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filterset_fields = ["products"]
